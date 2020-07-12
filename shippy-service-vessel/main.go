d03d912aea620c13e346c0cf1a655ac4d91dd880
Yurui Zhang <yuruiology@gmail.com>
2020-07-12 15:48:52 -0400

    basic vessel service?

--- shippy-service-vessel/main.go
+++ shippy-service-vessel/main.go
@@ -1,9 +1,66 @@
 package main
 
 import (
-	pb "github.com/pallymore/shippy/shippy/service-vessel/proto/vessel"
+	"context"
+	"errors"
+	"log"
+
+	"github.com/micro/go-micro"
+	pb "github.com/pallymore/shippy/shippy-service-vessel/proto/vessel"
 )
 
 type Repository interface {
 	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+}
+
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	for _, vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel, nil
+		}
+	}
+	return nil, errors.New("No vessel found by that spec")
+}
+
+type vesselService struct {
+	repo Repository
+}
+
+func (s *vesselService) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+	// Find the next available vessel
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return err
+	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
+}
+
+func main() {
+	// adds some vessels
+	vessels := []*pb.Vessel{
+		&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+	}
+	repo := &VesselRepository{vessels}
+
+	service := micro.NewService(
+		micro.Name("shippy.service.vessel"),
+	)
+
+	service.Init()
+
+	// Register our implementation with
+	if err := pb.RegisterVesselServiceHandler(service.Server(), &vesselService{repo}); err != nil {
+		log.Panic(err)
+	}
+
+	if err := service.Run(); err != nil {
+		log.Panic(err)
+	}
 }
