b595e4dd02872e6f1bd905b2995b0887938918c0
Yurui Zhang <yuruiology@gmail.com>
2020-07-11 20:48:59 -0400

    adds GetConsignments

--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -16,6 +16,7 @@ const port = ":50051"
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
@@ -31,6 +32,10 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	repo.consignments = updated
 	repo.mu.Unlock()
 	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
 }
 
 type service struct {
@@ -50,6 +55,11 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	// Return matching the `Response` message we created in our
 	// protobuf definition.
 	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
 }
 
 func main() {
